3b50d01381fc56b7bcd16b3834f987105e388ea1
schriftgestalt <georg.seifert@mac.com>
2017-11-08 09:42:44 +0100

    initial commit

---
+++ Backuper.glyphsPlugin/Contents/Resources/plugin.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	General Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/General%20Plugin
+#
+#
+###########################################################################################################
+
+
+from GlyphsApp.plugins import *
+import os
+
+class Backuper(GeneralPlugin):
+	def start(self):
+		Glyphs.addCallback(self.doBackup, DOCUMENTOPENED)
+	
+	def doBackup(self, sender):
+		document = sender.object()
+		importedVersion = document.valueForKey_("importedVersion")
+		if importedVersion != None and int(Glyphs.buildNumber) > int(importedVersion):
+			documentPath = document.fileURL().path()
+			fileName = os.path.basename(documentPath)
+			bachupFolder = os.path.join(os.path.dirname(documentPath), "Backup")
+			bachupPath = os.path.join(bachupFolder, importedVersion + "_" + fileName)
+			
+			fileManager = NSFileManager.defaultManager()
+			if fileManager.fileExistsAtPath_isDirectory_(bachupFolder, None) == (False, False):
+				if not fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error_(bachupFolder, True, None, None):
+					print "Could not make backup folder"
+			
+			if fileManager.isReadableFileAtPath_(documentPath):
+				NSFileManager.defaultManager().copyItemAtPath_toPath_error_(documentPath, bachupPath, None)
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
+	
